fd5b985090a5e5a9dd4a7994cba0c62af5865eb0
desarrolloclicandapp <desarrollo@clicandapp.com>
2026-01-07 14:49:15 -0300

    Implementacin de traduccion 1.1

--- admin-panel/src/main.jsx
+++ admin-panel/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
+import { BrandingProvider } from './context/BrandingContext'
 import { LanguageProvider } from './context/LanguageContext'
 
 createRoot(document.getElementById('root')).render(
