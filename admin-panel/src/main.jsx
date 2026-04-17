c4e4e964fda6ec8988b7105c5274ff370cb11301
nicovillalbaz <nico4villaz@gmail.com>
2026-04-17 13:31:49 -0300

    cambios de app y main jsx, y nuevos npm en packagejson, para google analytics

--- admin-panel/src/main.jsx
+++ admin-panel/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 import { BrandingProvider } from './context/BrandingContext'
 import { LanguageProvider } from './context/LanguageContext'
+
+ReactGA.initialize('G-6LS8CVYQE9')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
